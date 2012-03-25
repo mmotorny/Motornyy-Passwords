951a73d347444f1c26d459c9287b4d719ee4f849
Maksym Motornyy <mmotorny@gmail.com>
2012-03-26 00:53:37 +0200

    Made URL the first field, focused master password (second field) by default. URL value is initialized from active tab.

--- browser_action_popup.js
+++ browser_action_popup.js
@@ -1,15 +1,31 @@
 function onLoad() {
-  document.getElementsByClassName('fill-form-button')[0].addEventListener(
-    'click', function() {
-      var masterPasswordInput = 
-          document.getElementsByClassName('master-password-input')[0];
-      var masterPassword = masterPasswordInput.value;
-      console.log(masterPassword);
+  var urlInput = document.getElementsByClassName('url-input')[0];
+  var masterPasswordInput = 
+      document.getElementsByClassName('master-password-input')[0];
+  var fillFormButton = document.getElementsByClassName('fill-form-button')[0];
 
-      var urlInput = document.getElementsByClassName('url-input')[0];
-      var url = urlInput.value;
-      console.log(url);
+  urlInput.addEventListener('focus', function() {
+    masterPasswordInput.focus();
+  });
 
-      console.log(sjcl.misc.pbkdf2(masterPassword, sjcl.hash.sha256.hash(url)));
-    });
+  fillFormButton.addEventListener('click', function() {
+    var url = urlInput.value;
+    console.log(url);
+
+    var masterPassword = masterPasswordInput.value;
+    console.log(masterPassword);
+
+    console.log(sjcl.misc.pbkdf2(masterPassword, sjcl.hash.sha256.hash(url)));
+  });
+  
+  chrome.windows.getCurrent(function(currentWindow) {
+    chrome.tabs.query(
+      {
+        windowId: currentWindow.id,
+        active: true
+      },
+      function(tabs) {
+        urlInput.value = tabs[0].url;
+      });
+  });
 }
