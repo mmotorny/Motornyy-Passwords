e30c83608a0bb845f43217acb13c277d39e82bef
Maksym Motornyy <mmotorny@gmail.com>
2012-04-08 22:47:35 +0200

    Updating bit image on change in master password.

--- browser_action_popup.js
+++ browser_action_popup.js
@@ -1,37 +1,4 @@
-const POWER_2_32 = Math.pow(2, 32);
-const ZEROS_32 = new Array(32 + 1).join('0');
-
 function onLoad() {
-  var urlInput = document.getElementsByClassName('url-input')[0];
-  var masterPasswordInput = 
-      document.getElementsByClassName('master-password-input')[0];
-  var fillFormButton = document.getElementsByClassName('fill-form-button')[0];
-  var tabId;
-
-  fillFormButton.addEventListener('click', function() {
-    // TODO(maksym): Rename to tag.
-    var url = urlInput.value;
-    var masterPassword = masterPasswordInput.value;
-    var uniquePasswordBits = 
-        sjcl.misc.pbkdf2(masterPassword, sjcl.hash.sha256.hash(url)).
-            map(function(word) {
-              var binaryWord = (word >= 0 ? word : POWER_2_32 + word).
-                  toString(2);
-              return ZEROS_32.substr(0, 32 - binaryWord.length) + binaryWord;
-            }).join('');
-    
-    if (tabId === undefined) {
-      // TODO(maksym): Report error.
-    } else {
-      chrome.tabs.executeScript(tabId, {file: 'content_script.js'}, function() {
-        chrome.tabs.sendRequest(tabId, {
-          uniquePassword: new PasswordBuilder(uniquePasswordBits).build()
-        });
-        window.close();
-      });
-    }
-  });
-  
   chrome.windows.getCurrent(function(currentWindow) {
     chrome.tabs.query(
       {
@@ -39,15 +6,48 @@ function onLoad() {
         active: true
       },
       function(tabs) {
-        var tab = tabs[0];
-        tabId = tab.id;
-
-        var urlParser = document.createElement('a');
-        urlParser.href = tab.url;
-        urlInput.value = urlParser.host;
+        onLoadInTab(tabs[0]);
       });
   });
 }
+
+const POWER_2_32 = Math.pow(2, 32);
+const ZEROS_32 = new Array(32 + 1).join('0');
+
+function onLoadInTab(tab) {
+  var masterPasswordInput = document.getElementsByClassName(
+      'master-password-input')[0];
+  var confirmMasterPasswordInput = document.getElementsByClassName(
+      'confirm-master-password-input')[0];
+  var tagInput = document.getElementsByClassName('tag-input')[0];
+  var bitElements = document.getElementsByClassName('bit');
+  
+  masterPasswordInput.addEventListener('input', function() {
+    var passwordBits = 
+        sjcl.misc.pbkdf2(
+            masterPasswordInput.value, sjcl.hash.sha256.hash(tagInput.value)).
+                map(function(word) {
+                  var binaryWord = (word >= 0 ? word : POWER_2_32 + word).
+                      toString(2);
+                  return ZEROS_32.substr(0, 32 - binaryWord.length) + binaryWord;
+                }).join('');
+    
+    for (var bitIndex = 0; bitIndex < 256; ++bitIndex) {
+      var bitElement = bitElements[bitIndex];
+      bitElement.classList.remove('bit-zero');
+      bitElement.classList.remove('bit-one');
+      bitElement.classList.add(
+        passwordBits[bitIndex] == '0' ? 'bit-zero' : 'bit-one');
+    }
+  });
+}
+
+/*
+tabId = tab.id;
+var urlParser = document.createElement('a');
+urlParser.href = tab.url;
+tagInput.value = urlParser.host;
+*/
 
 function PasswordBuilder(bits) {
   var password = '';
@@ -81,3 +81,35 @@ function PasswordBuilder(bits) {
     return true;
   }
 }
+
+/*
+var tagInput = document.getElementsByClassName('tag-input')[0];
+var masterPasswordInput = 
+    document.getElementsByClassName('master-password-input')[0];
+var fillFormButton = document.getElementsByClassName('fill-form-button')[0];
+var tabId;
+
+fillFormButton.addEventListener('click', function() {
+  var tag = tagInput.value;
+  var masterPassword = masterPasswordInput.value;
+  var uniquePasswordBits = 
+      sjcl.misc.pbkdf2(masterPassword, sjcl.hash.sha256.hash(tag)).
+          map(function(word) {
+            var binaryWord = (word >= 0 ? word : POWER_2_32 + word).
+                toString(2);
+            return ZEROS_32.substr(0, 32 - binaryWord.length) + binaryWord;
+          }).join('');
+  
+  if (tabId === undefined) {
+    // TODO(maksym): Report error.
+  } else {
+    chrome.tabs.executeScript(tabId, {file: 'content_script.js'}, function() {
+      chrome.tabs.sendRequest(tabId, {
+        uniquePassword: new PasswordBuilder(uniquePasswordBits).build()
+      });
+      window.close();
+    });
+  }
+});
+*/
+
