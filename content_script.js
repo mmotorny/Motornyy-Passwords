61124454a112adf06f1c9ce3c7caa87e8ee82ca4
Maksym Motornyy <mmotorny@gmail.com>
2012-04-09 01:24:06 +0200

    "Fill form" button actually works.

--- content_script.js
+++ content_script.js
@@ -3,6 +3,13 @@ if (!injected) {
   injected = true;
 
   chrome.extension.onRequest.addListener(function(request) {
-    console.log(request.password);
+    var inputElements = document.getElementsByTagName('input');
+    for (var i = 0; i < inputElements.length; ++i) {
+      var inputElement = inputElements[i];
+      if (inputElement.type == 'password') {
+        inputElement.focus();
+        inputElement.value = request.password;
+      }
+    }
   });
 }
