d90b39eb1239ceb6d34d5ec711e69f2f79430a1e
Maksym Motornyy <mmotorny@gmail.com>
2012-04-09 00:03:22 +0200

    Fixed #4: confirm master password.

--- browser_action_popup.js
+++ browser_action_popup.js
@@ -6,81 +6,117 @@ function onLoad() {
         active: true
       },
       function(tabs) {
-        onLoadInTab(tabs[0]);
+        new Popup(tabs[0]);
       });
   });
 }
 
+function Popup(tab) {
+  this.tab = tab;
+  
+  this.masterPasswordInput = document.getElementsByClassName(
+      'master-password-input')[0];
+  this.confirmMasterPasswordInput = document.getElementsByClassName(
+      'confirm-master-password-input')[0];
+  this.masterPasswordMessage = document.getElementsByClassName(
+      'master-password-message')[0];
+  this.tagInput = document.getElementsByClassName('tag-input')[0];
+  this.bitElements = document.getElementsByClassName('bit');
+
+  var urlParser = document.createElement('a');
+  urlParser.href = tab.url;
+  this.tagInput.value = urlParser.host;
+
+  this.confirmMasterPassword();
+  this.updatePassword();
+
+  this.masterPasswordInput.addEventListener('input', function() {
+    this.confirmMasterPassword();
+    this.updatePassword();
+  }.bind(this));
+
+  this.confirmMasterPasswordInput.addEventListener('input', function() {
+    this.confirmMasterPassword();
+    this.updatePassword();
+  }.bind(this));
+  
+  this.tagInput.addEventListener('input', this.updatePassword.bind(this));
+}
+
+Popup.prototype.confirmMasterPassword = function() {
+  this.masterPasswordMessage.classList.remove('error-shown');
+  this.masterPasswordMessage.classList.remove('error-hidden');
+  
+  if (this.masterPasswordInput.value == '') {
+    this.masterPasswordMessage.innerText = 'Master password is empty.';
+    this.masterPasswordMessage.classList.add('error-shown');
+  } else if (this.masterPasswordInput.value != 
+      this.confirmMasterPasswordInput.value) {
+    this.masterPasswordMessage.innerText = 'Master passwords doesn\'t match.';
+    this.masterPasswordMessage.classList.add('error-shown');
+  } else {
+    this.masterPasswordMessage.classList.add('error-hidden');
+  }
+};
+
 const POWER_2_32 = Math.pow(2, 32);
 const ZEROS_32 = new Array(32 + 1).join('0');
 
-function onLoadInTab(tab) {
-  var masterPasswordInput = document.getElementsByClassName(
-      'master-password-input')[0];
-  var confirmMasterPasswordInput = document.getElementsByClassName(
-      'confirm-master-password-input')[0];
-  var tagInput = document.getElementsByClassName('tag-input')[0];
-  var bitElements = document.getElementsByClassName('bit');
-  
-  masterPasswordInput.addEventListener('input', function() {
-    var passwordBits = 
-        sjcl.misc.pbkdf2(
-            masterPasswordInput.value, sjcl.hash.sha256.hash(tagInput.value)).
-                map(function(word) {
-                  var binaryWord = (word >= 0 ? word : POWER_2_32 + word).
-                      toString(2);
-                  return ZEROS_32.substr(0, 32 - binaryWord.length) + binaryWord;
-                }).join('');
-    
-    for (var bitIndex = 0; bitIndex < 256; ++bitIndex) {
-      var bitElement = bitElements[bitIndex];
-      bitElement.classList.remove('bit-zero');
-      bitElement.classList.remove('bit-one');
-      bitElement.classList.add(
-        passwordBits[bitIndex] == '0' ? 'bit-zero' : 'bit-one');
-    }
-  });
-}
+Popup.prototype.updatePassword = function() {
+  var masterPassword = this.masterPasswordInput.value;
+  var tag = this.tagInput.value;
+  var passwordBits = 
+      sjcl.misc.pbkdf2(masterPassword, sjcl.hash.sha256.hash(tag)).
+          map(function(word) {
+            var binaryWord = (word >= 0 ? word : POWER_2_32 + word).
+                toString(2);
+            return ZEROS_32.substr(0, 32 - binaryWord.length) + binaryWord;
+          }).join('');
 
-/*
-tabId = tab.id;
-var urlParser = document.createElement('a');
-urlParser.href = tab.url;
-tagInput.value = urlParser.host;
-*/
+  for (var bitIndex = 0; bitIndex < 256; ++bitIndex) {
+    var bitElement = this.bitElements[bitIndex];
+    bitElement.classList.remove('bit-zero');
+    bitElement.classList.remove('bit-one');
+    bitElement.classList.add(
+      passwordBits[bitIndex] == '0' ? 'bit-zero' : 'bit-one');
+  }
+  
+  this.password = new PasswordBuilder(passwordBits).build();
+};
 
 function PasswordBuilder(bits) {
-  var password = '';
+  this.bits = bits;
+  this.password = '';
+}
 
-  this.build = function() {
-    while (true) {
-      if (!addCharacter('ABCDEFGHIJKLMNOP', 4)) {
-        break;
-      }
-      if (!addCharacter('abcdefghijklmnop', 4)) {
-        break;
-      }
-      if (!addCharacter('01234567', 3)) {
-        break;
-      }
-      if (!addCharacter('!@#$%^&*', 3)) {
-        break;
-      }
+PasswordBuilder.prototype.build = function() {
+  while (true) {
+    if (!this.addCharacter_('ABCDEFGHIJKLMNOP', 4)) {
+      break;
     }
-    
-    return password;
+    if (!this.addCharacter_('abcdefghijklmnop', 4)) {
+      break;
+    }
+    if (!this.addCharacter_('01234567', 3)) {
+      break;
+    }
+    if (!this.addCharacter_('!@#$%^&*', 3)) {
+      break;
+    }
   }
   
-  function addCharacter(characters, entropy) {
-    if (bits.length < entropy) {
-      return false;
-    }
-    
-    password += characters[parseInt(bits.substr(0, entropy), 2)];
-    bits = bits.substr(entropy);
-    return true;
+  return this.password;
+};
+
+PasswordBuilder.prototype.addCharacter_ = function(characters, entropy) {
+  if (this.bits.length < entropy) {
+    return false;
   }
-}
+  
+  this.password += characters[parseInt(this.bits.substr(0, entropy), 2)];
+  this.bits = this.bits.substr(entropy);
+  return true;
+};
 
 /*
 var tagInput = document.getElementsByClassName('tag-input')[0];
